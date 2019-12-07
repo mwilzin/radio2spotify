afdff6d41af80ad7e7763ea96cafacf6b7fd8b40
triggeredLife <49531800+triggeredLife@users.noreply.github.com>
2019-12-07 19:27:04 +0100

    Add files via upload

--- setup.js
+++ setup.js
@@ -11,7 +11,7 @@ var AccessToken = readlineSync.question('Please enter your Spotify Access Token 
 
 var RefreshToken = readlineSync.question('Please enter your Spotify Refresh Token ');
 
-var name = readlineSync.question('Please enter the name of the Radio Channel ');
+var name = readlineSync.question('Please enter the name of the Radio Channel ').replace(' ', '_');
 
 var uri = readlineSync.question('Please enter the URI of your Playlist (e.g. 3etIBt3wCSGny0MGgCwiHq) ');
 
@@ -31,15 +31,47 @@ if (!fs.existsSync(name)){
         console.log('Created script!');
       })
     });
+
     fs.appendFile(`${name}/settings.json`, settings, function (err) {
       if (err) throw err;
       console.log('Created settings!');
     })
+
     fs.appendFile(`${name}/last.json`, last, function (err) {
       if (err) throw err;
       console.log('Created file for the last song!');
     })
+
+    if (fs.existsSync("start.sh")){
+      fs.readFile('start.sh', 'utf8', function(err, contents) {
+        contents = contents.replace('done', '')
+        contents = contents.replace('  sleep 60', '')
+        contents = contents + `  echo - - - - - ${name} - - - - -\r\n  cd -\r\n  cd ${name}\r\n  node ${name}.js\r\n  sleep 60\r\ndone`
+        fs.unlink('start.sh', (err) => {
+          if(err) return console.log(err);
+          console.log('Deleted the file');
+        })
+        setTimeout(function () {
+          fs.appendFile('start.sh', contents, function (err) {
+            if (err) throw err;
+            console.log('Saved start file!');
+          })
+        }, 5000);
+      });
+    } else {
+      var startMuster = `while true\r\ndo\r\n  echo - - - - - ${name} - - - - -\r\n  cd -\r\n  cd ${name}\r\n  node ${name}.js\r\n  sleep 60\r\ndone`
+      fs.appendFile('start.sh', startMuster, function (err) {
+        if (err) throw err;
+        console.log('Saved start file!');
+      })
+    }
+
 } else {
   console.log('This name does already exist.');
   return;
 }
+
+// fs.unlink('start.sh', (err) => {
+//   if(err) return console.log(err);
+//   console.log('Deleted the start file');
+// })
