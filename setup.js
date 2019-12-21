c9e144755024b94a1255a795c7e03d97b157c203
triggeredLife <49531800+triggeredLife@users.noreply.github.com>
2019-12-21 13:37:06 +0100

    Update setup.js

--- setup.js
+++ setup.js
@@ -41,31 +41,6 @@ if (!fs.existsSync(name)){
       if (err) throw err;
       console.log('Created file for the last song!');
     })
-
-    if (fs.existsSync("start.sh")){
-      fs.readFile('start.sh', 'utf8', function(err, contents) {
-        contents = contents.replace('done', '')
-        contents = contents.replace('  sleep 60', '')
-        contents = contents + `  echo - - - - - ${name} - - - - -\r\n  cd -\r\n  cd ${name}\r\n  node ${name}.js\r\n  sleep 60\r\ndone`
-        fs.unlink('start.sh', (err) => {
-          if(err) return console.log(err);
-          console.log('Deleted the file');
-        })
-        setTimeout(function () {
-          fs.appendFile('start.sh', contents, function (err) {
-            if (err) throw err;
-            console.log('Saved start file!');
-          })
-        }, 5000);
-      });
-    } else {
-      var startMuster = `while true\r\ndo\r\n  echo - - - - - ${name} - - - - -\r\n  cd -\r\n  cd ${name}\r\n  node ${name}.js\r\n  sleep 60\r\ndone`
-      fs.appendFile('start.sh', startMuster, function (err) {
-        if (err) throw err;
-        console.log('Saved start file!');
-      })
-    }
-
 } else {
   console.log('This name does already exist.');
   return;
